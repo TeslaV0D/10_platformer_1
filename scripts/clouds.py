1cf9f80282a87eee0524e2e9f30bba295a509c85
Anton <anton.mischenko321@gmail.com>
2025-12-17 13:25:39 +0100

    New sounds, fixes and much more...

--- scripts/clouds.py
+++ scripts/clouds.py
@@ -6,17 +6,17 @@ class Cloud:
         self.img = img
         self.speed = speed
         self.depth = depth
-        
+    
     def update(self):
         self.pos[0] += self.speed
-    
-    def render(self, surf, offset = (0, 0)):
+        
+    def render(self, surf, offset=(0, 0)):
         render_pos = (self.pos[0] - offset[0] * self.depth, self.pos[1] - offset[1] * self.depth)
         surf.blit(self.img, (render_pos[0] % (surf.get_width() + self.img.get_width()) - self.img.get_width(), 
                              render_pos[1] % (surf.get_height() + self.img.get_height()) - self.img.get_height()))
-    
+        
 class Clouds:
-    def __init__(self, cloud_images, count = 16):
+    def __init__(self, cloud_images, count=16):
         self.clouds = []
         
         for i in range(count):
@@ -29,6 +29,6 @@ class Clouds:
         for cloud in self.clouds:
             cloud.update()
     
-    def render(self, surf, offset = (0, 0)):
+    def render(self, surf, offset=(0, 0)):
         for cloud in self.clouds:
             cloud.render(surf, offset=offset)
